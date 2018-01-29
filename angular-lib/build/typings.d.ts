88c7e51440a009c81cd84ec64a3fde0082718fbb
Chase Gibbons <chasegibbons@Chase-Gibbons-MacBook-Pro.local>
2018-01-28 23:58:11 -0700

    deleted ./angular-lib

--- angular-lib/build/typings.d.ts
+++
@@ -1,1 +0,0 @@
-declare var hello_world: any;
--- angular-lib/dist/lib/loading-indicator/loading-indicator.component.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnInit, OnDestroy } from '@angular/core';
-export declare class LoadingIndicatorComponent implements OnInit, OnDestroy {
-    height: any;
-    width: any;
-    src: string;
-    constructor();
-    ngOnInit(): void;
-    ngOnDestroy(): void;
-}
